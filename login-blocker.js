31c7ac827e2b86dd562ec67718893d958d8a5d11
weaselflink <stefan@stefanbissell.de>
2022-03-08 11:55:39 +0100

    keep log in layer hidden

--- login-blocker.js
+++ login-blocker.js
@@ -1,31 +1,40 @@
-
 const logPrefix = '>>>>>>>>>> Twitter Login Blocker';
 console.log(`${logPrefix} running`);
 
 const htmlElement = document.querySelector("html");
 const layersElement = document.querySelector("div#layers");
 
-const isLoginLayer = function(node) {
+const isLoginLayer = function (node) {
     const spans = node.querySelectorAll('span')
     return [...spans].filter(e => e.textContent === 'Log in').length > 0
 }
 
-const htmlCallback = function(mutationsList) {
-    for(const mutation of mutationsList) {
+const htmlCallback = function (mutationsList) {
+    for (const mutation of mutationsList) {
         if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
             mutation.target.style.overflow = null;
         }
     }
 };
 
-const layersCallback = function(mutationsList) {
-    for(const mutation of mutationsList) {
+const keepHiddenCallback = function (mutationsList) {
+    for (const mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+            mutation.target.style.display = 'none';
+        }
+    }
+};
+
+const layersCallback = function (mutationsList) {
+    for (const mutation of mutationsList) {
         if (mutation.type === 'childList' || mutation.type === 'subtree') {
             const loginLayers = [...mutation.addedNodes]
                 .filter(node => isLoginLayer(node))
 
             loginLayers.forEach(node => {
                 console.log(`${logPrefix} login layer blocked`);
+                const keepHiddenObserver = new MutationObserver(keepHiddenCallback);
+                keepHiddenObserver.observe(node, {attributeFilter: ['style']});
                 return node.style.display = 'none';
             });
         }
@@ -33,13 +42,7 @@ const layersCallback = function(mutationsList) {
 };
 
 const htmlObserver = new MutationObserver(htmlCallback);
-htmlObserver.observe(
-    htmlElement,
-    { attributes: true }
-);
+htmlObserver.observe(htmlElement, {attributeFilter: ['style']});
 
 const layersObserver = new MutationObserver(layersCallback);
-layersObserver.observe(
-    layersElement,
-    { childList: true }
-);
+layersObserver.observe(layersElement, {childList: true});
